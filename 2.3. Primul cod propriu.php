26a3cf28bfce8477fe1f08e1ff39d876faa98193
Sergiu <mansergiumihai@gmail.com>
2017-06-21 19:19:54 +0300

    .

--- 2.3. Primul cod propriu.php
+++ 2.3. Primul cod propriu.php
@@ -2,9 +2,9 @@
 $pi = 3.1415;
 echo 'Valoarea lui PI este ',$pi;
 
+//Sintaxa alternativă pentru echo*
+//Raspuns: 1. Echo va fi apelat de trei ori.
 
-//1. Echo va fi apelat de trei ori.
 
-
-
-//Echo va fi apelat o singura data. Mai intai se va salva valoarea 'Hello world' in variabila $foo dupa care se va afisa valoarea salavata in variabila $foo.
+//Determinarea fluxului de execuție într-un exemplu simplu 
+// Raspuns: Echo va fi apelat o singura data. Mai intai se va salva valoarea 'Hello world' in variabila $foo dupa care se va afisa valoarea salavata in variabila $foo.

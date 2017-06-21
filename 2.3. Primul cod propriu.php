29c08e773c3963a630f5b308ec1bd21a07b2f60a
Sergiu <mansergiumihai@gmail.com>
2017-06-21 17:53:24 +0300

    .

---
+++ 2.3. Primul cod propriu.php
@@ -0,0 +1,10 @@
+<?php
+$pi = 3.1415;
+echo 'Valoarea lui PI este ',$pi;
+
+
+//1. Echo va fi apelat de trei ori.
+
+
+
+// In primul rand, se atribuie variabilei foo valoarea hello world care este un string, dupa care se printeaza variabila. Echo va fi apelat o singura data.

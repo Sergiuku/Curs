c085b7f891a1e36aaf8848d3af9e7619d100ea2e
Sergiu <mansergiumihai@gmail.com>
2017-06-21 21:07:03 +0300

    .

---
+++ 2.5 Am sintetizat corect notiunea de expresie.php
@@ -0,0 +1,14 @@
+<?php
+ //Partea I:
+echo 'Comparatie dintre expresie matematica si un string: ', var_dump((2*3)+5 === 'wrong'), '<br>';
+
+
+
+// Partea II:
+
+/*
+ *Raspuns: Se face o comparatie dintre un int(4) si un string('5'). Aceasta comparatie va fi falsa.
+ *			Comparatia dintre cele 2 va fi valoarea data variabilei $foo.
+ */
+
+
---
+++ 2.5. Verifica-ti puterea de a face analogii.php
@@ -0,0 +1,7 @@
+<?php
+
+$bool = 'down';
+echo 'Comparatie int si string: ',var_dump(32 === 'no'), '<br>';
+echo 'Comparatie float si int: ',var_dump(43 === 3.432), '<br>';
+echo 'Comparatie string si float: ',var_dump('43.232' === 43.232), '<br>';
+echo 'Comparatie bool si string: ', var_dump($bool === 'up'), '<br>';

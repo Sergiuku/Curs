1da006debe2d6a7e1f0c5fbfd1afa1095324a8b2
Sergiu <mansergiumihai@gmail.com>
2017-06-21 19:23:59 +0300

    .

---
+++ 2.3.3 Lipsa Output.php
@@ -0,0 +1,5 @@
+
+<?php
+$a='Hello ';
+$a.='world ';
+// Raspuns: Deoarece lipseste echo pentru afisare.
--- 2.3.3. Operatii matematice.php
+++
@@ -1,7 +0,0 @@
-/*
-De ce codul următor nu afișează nimic?
-<?php
-$a='Hello ';
-$a.='world ';
-Raspuns: Deoarece lipseste echo pentru afisare.
-*/

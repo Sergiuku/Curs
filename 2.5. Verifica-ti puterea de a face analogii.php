006b209c46836d18c32ed9642bf8f95deebd234e
Sergiu <mansergiumihai@gmail.com>
2017-06-26 18:35:01 +0300

    .

--- 2.5. Verifica-ti puterea de a face analogii.php
+++ 2.5. Verifica-ti puterea de a face analogii.php
@@ -1,6 +1,6 @@
 <?php
 
-$bool = 'down';
+$bool = TRUE;
 echo 'Comparatie int si string: ',var_dump(32 === 'no'), '<br>';
 echo 'Comparatie float si int: ',var_dump(43 === 3.432), '<br>';
 echo 'Comparatie string si float: ',var_dump('43.232' === 43.232), '<br>';

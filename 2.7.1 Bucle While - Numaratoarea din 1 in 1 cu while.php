78392ef1790d89d5efd2d5cb084fddf91ab922b4
Sergiu <mansergiumihai@gmail.com>
2017-06-29 19:22:41 +0300

    .

--- 2.7.1 Bucle While - Numaratoarea din 1 in 1 cu while.php
+++ 2.7.1 Bucle While - Numaratoarea din 1 in 1 cu while.php
@@ -8,6 +8,5 @@ while($var <= 10) {
 }
 
 /* Partea II
- * Variabila $var va lua pe rand valoarea de la 1 la 10.
- * De fiecare data cand bucla se repeta va avea o valoarea crescatoare: 1,2,3,4,5,6,7,8,9,10.
+ * Variabila $var va lua pe rand valoarea de la 1 la 11.
  */

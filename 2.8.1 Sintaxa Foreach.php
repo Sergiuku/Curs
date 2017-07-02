c70be09b608a1b4bf488d17a66ba485f0c3da9d3
Sergiu <mansergiumihai@gmail.com>
2017-07-02 21:19:11 +0300

    .

---
+++ 2.8.1 Sintaxa Foreach.php
@@ -0,0 +1,9 @@
+<?php
+$city = array (
+        'paris' => 'eiffel',
+        'london' => 'big bang',
+        'roma' => 'colosseum'
+);
+foreach($city as $index => $place) {
+    echo 'Elementul ',$index ,' are valoarea ', $place,'. <br>';
+}

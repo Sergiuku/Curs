f9b9a1207461b2978a83aedb06bd378a9e2e152f
Sergiu <mansergiumihai@gmail.com>
2017-07-04 22:15:33 +0300

    .

--- 2.8 Meniu de navigare PARTEA I si II.php
+++ 2.8 Meniu de navigare PARTEA I si II.php
@@ -2,15 +2,13 @@
 $menu = array('Home', 'Contact', 'Despre');
 if(TRUE === empty($menu)) {
 	echo 'Meniu Inexistent.';
+}
 ?>
-
 <ul>
 <?php
-    foreach ($menu as $category) {
-        echo "<li><a href='#$category'>$category</li>";
-    }
+foreach ($menu as $category) {
+	echo "<li><a href='#$category'>$category</li>";
+}
 
 ?>
 </ul>
-
-

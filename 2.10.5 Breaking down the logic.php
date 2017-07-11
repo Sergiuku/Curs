31c8a81464a886cf0cf71f2840379a6deb982690
Sergiu <mansergiumihai@gmail.com>
2017-07-11 18:44:41 +0300

    .

---
+++ 2.10.5 Breaking down the logic.php
@@ -0,0 +1,20 @@
+A B A v B
+T T   T
+T F   T
+F T   T
+F F   F
+
+<?php
+if(TRUE) {
+    if(TRUE) {
+        echo "TRUE || TRUE = TRUE";
+    } else {
+        echo "TRUE || FALSE = TRUE";
+    }
+} else {
+    if(FALSE) {
+        echo "FALSE || TRUE = TRUE";
+    } else {
+        echo "FALSE || FALSE = FALSE";
+    }
+}

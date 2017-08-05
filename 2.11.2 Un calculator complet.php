bd06e2eb79f8376f92b5f3c909285297213487ab
Sergiu <mansergiumihai@gmail.com>
2017-08-05 13:47:19 +0300

    .

--- 2.11.2 Un calculator complet.php
+++ 2.11.2 Un calculator complet.php
@@ -1,46 +1,35 @@
 <?php
-
 if (isset($_GET['a']) && isset($_GET['b'])) {
-    $op = NULL;
+    $op = 'none';
     if (isset($_GET['op'])) {
         $op = $_GET['op'];
     }
     switch ($op) {
-        case NULL:
+        case 'none':
         case 'add':
-            if ($op === 'add') {
-                echo (int)$_GET['a'], ' + ', (int)$_GET['b'], ' = ', (int)$_GET['a'] + (int)$_GET['b'], '<br>';
-                if (isset($op)) {
-                    break;
-                }
+            echo (int)$_GET['a'], ' + ', (int)$_GET['b'], ' = ', (int)$_GET['a'] + (int)$_GET['b'], '<br>';
+            if ($op != 'none') {
+                break;
             }
         case 'sub':
-            if ($op === 'sub') {
-                echo (int)$_GET['a'], ' - ', (int)$_GET['b'], ' = ', (int)$_GET['a'] - (int)$_GET['b'], '<br>';
-                if (isset($op)) {
-                    break;
-                }
+            echo (int)$_GET['a'], ' - ', (int)$_GET['b'], ' = ', (int)$_GET['a'] - (int)$_GET['b'], '<br>';
+            if ($op != 'none') {
+                break;
             }
         case 'mul':
-            if ($op === 'mul') {
-                echo (int)$_GET['a'], ' * ', (int)$_GET['b'], ' = ', (int)$_GET['a'] * (int)$_GET['b'], '<br>';
-                if (isset($op)) {
-                    break;
-                }
+            echo (int)$_GET['a'], ' * ', (int)$_GET['b'], ' = ', (int)$_GET['a'] * (int)$_GET['b'], '<br>';
+            if ($op != 'none') {
+                break;
             }
         case 'div':
-            if ($op === 'div') {
-                echo (int)$_GET['a'], ' / ', (int)$_GET['b'], ' = ', (int)$_GET['a'] / (int)$_GET['b'], '<br>';
-                if (isset($op)) {
-                    break;
-                }
+            echo (int)$_GET['a'], ' / ', (int)$_GET['b'], ' = ', (int)$_GET['a'] / (int)$_GET['b'], '<br>';
+            if ($op != 'none') {
+                break;
             }
         case 'mod':
-            if ($op === 'mod') {
-                echo (int)$_GET['a'], ' % ', (int)$_GET['b'], ' = ', (int)$_GET['a'] % (int)$_GET['b'], '<br>';
-                if (isset($op)) {
-                    break;
-                }
+            echo (int)$_GET['a'], ' % ', (int)$_GET['b'], ' = ', (int)$_GET['a'] % (int)$_GET['b'], '<br>';
+            if ($op != 'none') {
+                break;
             }
         default:
             echo "Trebuie specificat unul dintre urmatorii operatori: add,sub,mult,div,mod", "<br>";

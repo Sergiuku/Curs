ccb1953c5fe6e290b19713995fc7cdecdc64b552
Sergiu <mansergiumihai@gmail.com>
2017-08-05 17:16:53 +0300

    .

--- 2.12.View Logic si Business Logic.php
+++ 2.12.View Logic si Business Logic.php
@@ -2,22 +2,22 @@
 if (isset($_POST['a']) && isset($_POST['b']) && isset($_POST['submit'])) {
     switch ($_POST['op']) {
         case 'add':
-            echo (int)$_POST['a'], ' + ', (int)$_POST['b'], ' = ', (int)$_POST['a'] + (int)$_POST['b'], '<br>';
+            echo $_POST['a'], ' + ', $_POST['b'], ' = ', $_POST['a'] + $_POST['b'], '<br>';
             break;
         case 'sub':
-            echo (int)$_POST['a'], ' - ', (int)$_POST['b'], ' = ', (int)$_POST['a'] - (int)$_POST['b'], '<br>';
+            echo $_POST['a'], ' - ', $_POST['b'], ' = ', $_POST['a'] - $_POST['b'], '<br>';
             break;
         case 'mul':
-            echo (int)$_POST['a'], ' * ', (int)$_POST['b'], ' = ', (int)$_POST['a'] * (int)$_POST['b'], '<br>';
+            echo $_POST['a'], ' * ', $_POST['b'], ' = ', $_POST['a'] * $_POST['b'], '<br>';
             break;
         case 'div':
-            echo (int)$_POST['a'], ' / ', (int)$_POST['b'], ' = ', (int)$_POST['a'] / (int)$_POST['b'], '<br>';
+            echo $_POST['a'], ' / ', $_POST['b'], ' = ', $_POST['a'] / $_POST['b'], '<br>';
             break;
         case 'mod':
-            echo (int)$_POST['a'], ' % ', (int)$_POST['b'], ' = ', (int)$_POST['a'] % (int)$_POST['b'], '<br>';
+            echo $_POST['a'], ' % ', $_POST['b'], ' = ', $_POST['a'] % $_POST['b'], '<br>';
             break;
         default:
-            echo 'Trebuie specificat a, b sau operatorul.', "<br>";
+            echo 'Trebuie specificat unul dintre numere sau operatul.', "<br>";
             break;
     }
 }
@@ -26,10 +26,10 @@ if (isset($_POST['a']) && isset($_POST['b']) && isset($_POST['submit'])) {
 <body>
 <h2>Calculator</h2>
 <form method="POST">
-    <label for="a">Numar a:</label>
-    <input type="text" name="a" id="a"/>
-    <label for="b">Numar b:</label>
-    <input type="text" name="b" id="b"/>
+    <label for="a">Numar:</label>
+    <input type="number" name="a" id="a"/>
+    <label for="b">Numar:</label>
+    <input type="number" name="b" id="b"/>
     <label for="op">Specifica operatia(add,sub,mul,div,mod):</label>
     <input type="text" name="op" id="op"/>
     <input type="submit" name="submit" value="Calculeaza"/>

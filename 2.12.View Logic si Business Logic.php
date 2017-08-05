9a0e726775764950804538ad666ed045ac3c8f07
Sergiu <mansergiumihai@gmail.com>
2017-08-05 17:34:39 +0300

    .

--- 2.12.View Logic si Business Logic.php
+++ 2.12.View Logic si Business Logic.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_POST['a']) && isset($_POST['b']) && isset($_POST['submit'])) {
-    $a = $_POST['a'];
-    $b = $_POST['b'];
+    $a= (int)$_POST['a'];
+    $b= (int)$_POST['b'];
     switch ($_POST['op']) {
         case 'add':
             echo $a, ' + ', $b, ' = ', $a + $b, '<br>';
@@ -29,9 +29,9 @@ if (isset($_POST['a']) && isset($_POST['b']) && isset($_POST['submit'])) {
 <h2>Calculator</h2>
 <form method="POST">
     <label for="a">Numar:</label>
-    <input type="number" name="a" id="a"/>
+    <input type="text" name="a" id="a"/>
     <label for="b">Numar:</label>
-    <input type="number" name="b" id="b"/>
+    <input type="text" name="b" id="b"/>
     <label for="op">Specifica operatia(add,sub,mul,div,mod):</label>
     <input type="text" name="op" id="op"/>
     <input type="submit" name="submit" value="Calculeaza"/>

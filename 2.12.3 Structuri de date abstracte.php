2c9a23cbd7f40599e990e2e8dc8a1bc7bfe8ae42
Sergiu <mansergiumihai@gmail.com>
2017-08-05 14:23:16 +0300

    .

--- 2.12.3 Structuri de date abstracte.php
+++ 2.12.3 Structuri de date abstracte.php
@@ -4,13 +4,13 @@ $fructe = array(
     'pere ' => 'Perele sunt bogate in vitamina C si in potasiu.',
     'alune ' => 'Alunele sunt bogate in proteine , grasimi nesaturate si vitamina B6.'
 );
-if (isset($_GET['submit']) && isset($_GET['fructe'])) {
-    foreach ($_GET['fructe'] as $fruct) {
+if (isset($_POST['submit']) && isset($_POST['fructe'])) {
+    foreach ($_POST['fructe'] as $fruct) {
         echo '<p>', $fructe[$fruct], '</p>';
     }
 }
 ?>
-<form method="GET">
+<form method="POST">
     <?php
     foreach ($fructe as $fruct => $value) {
         echo '<input type="checkbox" name="fructe[]" value="' . $fruct . '" id="fruct-id"/><label for="fruct-id">' . $fruct . '</label>';

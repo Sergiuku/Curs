247a031799c3c99315ef1338c40766b5164e1966
Sergiu <mansergiumihai@gmail.com>
2017-08-05 16:17:50 +0300

    .

--- 2.12.View Logic si Business Logic.php
+++ 2.12.View Logic si Business Logic.php
@@ -1,43 +1,40 @@
 <?php
 if (isset($_POST['submit']) && !empty(isset($_POST['submit']))) {
     if (isset($_POST['a']) && isset($_POST['b']) && !empty($_POST['a']) && !empty($_POST['b'])) {
-        $op = NULL;
+        $op = 'none';
         if (isset($_POST['op'])) {
             $op = $_POST['op'];
         }
         switch ($op) {
-            case NULL:
-            case '':
-                if (trim($op) === '') {
-                    $op = NULL;
-                }
+            Case 'none':
             case 'add':
                 echo (int)$_POST['a'], ' + ', (int)$_POST['b'], ' = ', (int)$_POST['a'] + (int)$_POST['b'], '<br>';
-                if (isset($op)) {
+                if ($op != 'none') {
                     break;
                 }
-
             case 'sub':
                 echo (int)$_POST['a'], ' - ', (int)$_POST['b'], ' = ', (int)$_POST['a'] - (int)$_POST['b'], '<br>';
-                if (isset($op)) {
+                if ($op != 'none') {
                     break;
                 }
             case 'mul':
-                echo (int)$_GET['a'], ' * ', (int)$_GET['b'], ' = ', (int)$_GET['a'] * (int)$_GET['b'], '<br>';
-                if (isset($op)) {
+                echo (int)$_POST['a'], ' * ', (int)$_POST['b'], ' = ', (int)$_POST['a'] * (int)$_POST['b'], '<br>';
+                if ($op != 'none') {
                     break;
                 }
             case 'div':
                 echo (int)$_POST['a'], ' / ', (int)$_POST['b'], ' = ', (int)$_POST['a'] / (int)$_POST['b'], '<br>';
-                if (isset($op)) {
+                if ($op != 'none') {
                     break;
                 }
             case 'mod':
                 echo (int)$_POST['a'], ' % ', (int)$_POST['b'], ' = ', (int)$_POST['a'] % (int)$_POST['b'], '<br>';
-                if (isset($op)) {
+                if ($op != 'none') {
                     break;
                 }
-
+            default:
+                echo "Trebuie specificat unul dintre urmatorii operatori: add,sub,mult,div,mod", "<br>";
+                break;
         }
     } else {
         echo 'Introdu valorile a si b.';
@@ -50,9 +47,9 @@ if (isset($_POST['submit']) && !empty(isset($_POST['submit']))) {
 <form method="POST">
     <label for="a">Numar:</label>
     <input type="text" name="a" id="a"/>
-    <label for="a">Numar:</label>
+    <label for="b">Numar:</label>
     <input type="text" name="b" id="b"/>
-    <label for="a">Specifica operatia(add,sub,mul,div,mod):</label>
+    <label for="op">Specifica operatia(add,sub,mul,div,mod):</label>
     <input type="text" name="op" id="op"/>
     <input type="submit" name="submit" value="Calculeaza"/>
     </select>

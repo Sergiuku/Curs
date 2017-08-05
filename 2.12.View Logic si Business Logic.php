19a8f3542f7336a924e60d6ebfab8e27604bd6c4
Sergiu <mansergiumihai@gmail.com>
2017-08-05 17:01:11 +0300

    .

--- 2.12.View Logic si Business Logic.php
+++ 2.12.View Logic si Business Logic.php
@@ -17,7 +17,7 @@ if (isset($_POST['a']) && isset($_POST['b']) && isset($_POST['submit'])) {
             echo (int)$_POST['a'], ' % ', (int)$_POST['b'], ' = ', (int)$_POST['a'] % (int)$_POST['b'], '<br>';
             break;
         default:
-            echo 'Trebuie specificat a,b sau operatorul.', "<br>";
+            echo 'Trebuie specificat a, b sau operatorul.', "<br>";
             break;
     }
 }
@@ -26,9 +26,9 @@ if (isset($_POST['a']) && isset($_POST['b']) && isset($_POST['submit'])) {
 <body>
 <h2>Calculator</h2>
 <form method="POST">
-    <label for="a">Numar:</label>
+    <label for="a">Numar a:</label>
     <input type="text" name="a" id="a"/>
-    <label for="b">Numar:</label>
+    <label for="b">Numar b:</label>
     <input type="text" name="b" id="b"/>
     <label for="op">Specifica operatia(add,sub,mul,div,mod):</label>
     <input type="text" name="op" id="op"/>

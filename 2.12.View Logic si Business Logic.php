20e9a4e814e1a1ace2ac961688feb5f346bba493
Sergiu <mansergiumihai@gmail.com>
2017-08-05 17:25:42 +0300

    .

--- 2.12.View Logic si Business Logic.php
+++ 2.12.View Logic si Business Logic.php
@@ -1,23 +1,25 @@
 <?php
 if (isset($_POST['a']) && isset($_POST['b']) && isset($_POST['submit'])) {
+    $a= $_POST['a'];
+    $b= $_POST['b'];
     switch ($_POST['op']) {
         case 'add':
-            echo $_POST['a'], ' + ', $_POST['b'], ' = ', $_POST['a'] + $_POST['b'], '<br>';
+            echo $a, ' + ', $b, ' = ', $a + $b, '<br>';
             break;
         case 'sub':
-            echo $_POST['a'], ' - ', $_POST['b'], ' = ', $_POST['a'] - $_POST['b'], '<br>';
+            echo $a, ' - ', $b, ' = ', $a - $b, '<br>';
             break;
         case 'mul':
-            echo $_POST['a'], ' * ', $_POST['b'], ' = ', $_POST['a'] * $_POST['b'], '<br>';
+            echo $a, ' * ', $b, ' = ', $a * $b, '<br>';
             break;
         case 'div':
-            echo $_POST['a'], ' / ', $_POST['b'], ' = ', $_POST['a'] / $_POST['b'], '<br>';
+            echo $a, ' / ', $b, ' = ', $a / $b, '<br>';
             break;
         case 'mod':
-            echo $_POST['a'], ' % ', $_POST['b'], ' = ', $_POST['a'] % $_POST['b'], '<br>';
+            echo $a, ' % ', $b, ' = ', $a % $b, '<br>';
             break;
         default:
-            echo 'Trebuie specificat unul dintre numere sau operatorul.', "<br>";
+            echo 'Trebuie specificat unul dintre numere sau operatul.', "<br>";
             break;
     }
 }
@@ -26,9 +28,9 @@ if (isset($_POST['a']) && isset($_POST['b']) && isset($_POST['submit'])) {
 <body>
 <h2>Calculator</h2>
 <form method="POST">
-    <label for="a">Numar a:</label>
+    <label for="a">Numar:</label>
     <input type="number" name="a" id="a"/>
-    <label for="b">Numar b:</label>
+    <label for="b">Numar:</label>
     <input type="number" name="b" id="b"/>
     <label for="op">Specifica operatia(add,sub,mul,div,mod):</label>
     <input type="text" name="op" id="op"/>

f53424b58ab819af41a3f048e564081fadd6e390
Sergiu <mansergiumihai@gmail.com>
2017-08-15 20:59:08 +0300

    .

--- 2.14.Operatii pe biti.php
+++ 2.14.Operatii pe biti.php
@@ -8,6 +8,6 @@ for ($b = 1; $b <= 8 * PHP_INT_SIZE; $b++) {
     echo 'a^b=', $a ^ $b;
     echo '~a=', ~$a;
     echo '~b=', ~$b;
-    echo '(a>>b)&0x1=', ($a >> $b) & 0x1,
+    echo '(a>>b)&0x1=', ($a >> $b) & 0x1;
     echo '<br>';
 }

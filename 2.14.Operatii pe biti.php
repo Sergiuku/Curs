379e37ae8296bb489d8480206e1a38e9587ac7e4
Sergiu <mansergiumihai@gmail.com>
2017-08-15 20:58:20 +0300

    .

--- 2.14.Operatii pe biti.php
+++ 2.14.Operatii pe biti.php
@@ -1,13 +1,13 @@
 <?php
 $a = 5;
-for($b=1; $b<=8*PHP_INT_SIZE; $b++) {
-    echo 'a&b=', $a & $b,'; ',
-        'a|b=', $a | $b,'; ',
-        'a<<b=', $a << $b,'; ',
-        'a>>b=', $a >> $b,'; ',
-        'a^b=', $a ^ $b,'; ',
-        '~a=', ~$a,'; ',
-        '~b=', ~$b,'; ',
-        '(a>>b)&0x1=', ($a >> $b) & 0x1,
-    '<br>';
+for ($b = 1; $b <= 8 * PHP_INT_SIZE; $b++) {
+    echo 'a&b=', $a & $b;
+    echo 'a|b=', $a | $b;
+    echo 'a<<b=', $a << $b;
+    echo 'a>>b=', $a >> $b;
+    echo 'a^b=', $a ^ $b;
+    echo '~a=', ~$a;
+    echo '~b=', ~$b;
+    echo '(a>>b)&0x1=', ($a >> $b) & 0x1,
+    echo '<br>';
 }

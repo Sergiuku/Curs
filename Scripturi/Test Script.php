53778626ddd382449e9821a6b6f58bf8ed286392
Sergiu <mansergiumihai@gmail.com>
2017-09-03 14:50:56 +0300

    .

--- Scripturi/Test Script.php
+++ Scripturi/Test Script.php
@@ -6,18 +6,22 @@ $words = array(
 );
 // Afisarea random a cuvintelor
 $word = $words[array_rand($words)];
-echo 'Cuvantul este un obiect, alcatuit din:';
-// Afisarea lungimii cuvantului in liniute
-for ($i = 0; $i < strlen($word); $i++) {
-    echo ' _ ';
-}
-echo ' litere'."<br>";
 // Daca se ghiceste litera sa se afiseze.
 if (isset($_POST['letter']) && isset($_POST['submit'])) {
     $letter = $_POST['letter'];
-    if(strpos($word, $letter) !== false) {
-        echo 'Ai ghicit litera: '.$letter;
+    $left = 0;
+    $show = '';
+    for ($i = 0; $i < strlen($word); ++$i) {
+        if (strpos($word[$i], $letter) !== FALSE) {
+            $show .= $word[$i];
+        } else {
+            $show .= '_';
+            ++$left;
+        }
+        $show .= ' ';
     }
+    $show = trim($show);
+    echo 'Cuvantul este: '.$show;
 }
 ?>
 
@@ -29,7 +33,7 @@ if (isset($_POST['letter']) && isset($_POST['submit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
-<form method="post" action="index.php">
+<form method="post" action="add.php">
     <fieldset>
         <legend>Ghiceste cuvantul:</legend>
         Introdu litera: <input type="text" name="letter"/>

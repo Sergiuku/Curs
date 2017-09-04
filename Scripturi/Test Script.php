fe29bc142ded223db3f9a11d3fb13c7e12c487f2
Sergiu <mansergiumihai@gmail.com>
2017-09-04 19:56:59 +0300

    .

--- Scripturi/Test Script.php
+++ Scripturi/Test Script.php
@@ -4,10 +4,22 @@ $words = array(
     'cal',
     'cas'
 );
+$keep = array(
+    'c',
+    'a',
+    's',
+    'l'
+);
 // Afisarea random a cuvintelor
 $word = $words[array_rand($words)];
 // Daca se ghiceste litera sa se afiseze.
 if (isset($_POST['letter']) && isset($_POST['submit'])) {
+    if(in_array($_POST['letter'], $keep)) {
+        echo $_POST['hidden1'];
+    } else if(in_array($_POST['letter'], $keep[1])) {
+        echo $_POST['hidden2'];
+    }
+
     $letter = $_POST['letter'];
     $left = 0;
     $show = '';
@@ -21,7 +33,7 @@ if (isset($_POST['letter']) && isset($_POST['submit'])) {
         $show .= ' ';
     }
     $show = trim($show);
-    echo 'Cuvantul este: '.$show;
+    echo 'Cuvantul este: ' . $show;
 }
 ?>
 
@@ -37,6 +49,10 @@ if (isset($_POST['letter']) && isset($_POST['submit'])) {
     <fieldset>
         <legend>Ghiceste cuvantul:</legend>
         Introdu litera: <input type="text" name="letter"/>
+        <input type="hidden" name="hidden1" value="c"/>
+        <input type="hidden" name="hidden2" value="a"/>
+        <input type="hidden" name="hidden3" value="s'"/>
+        <input type="hidden" name="hidden" value="l"/>
         <input type="submit" name="submit" value="Ghiceste!"/>
     </fieldset>
 </form>
